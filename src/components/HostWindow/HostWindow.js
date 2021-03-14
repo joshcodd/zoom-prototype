8e86816fe17638a16726e310f5e0ebce987ade6b
Josh Codd <jjc21@live.co.uk>
2021-03-14 14:42:12 +0000

    Change file name

--- src/components/HostWindow/HostWindow.js
+++ src/components/HostWindow/HostWindow.js
@@ -4,7 +4,7 @@ function HostWindow() {
   return (
     <div className="screenContainer">
       <img
-        src={process.env.PUBLIC_URL + "/screen-min.png"}
+        src={process.env.PUBLIC_URL + "/screen.png"}
         className="screen"
         alt="Alternative Text"
       />
